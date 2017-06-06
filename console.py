033d6173daea458f61d493beee0b107002a5262f
Kimberly Wong <kjowong@gmail.com>
2017-06-06 16:23:16 +0000

    Adding partial update w/0 type

--- console.py
+++ console.py
@@ -37,32 +37,12 @@ class HBNBCommand(cmd.Cmd):
                 print("** class doesn't exist **")
 
     def do_show(self, line):
-        """
-        if not line:
-            print("** class name missing **")
-        else:
-            s = line.split()
-
-            if not s[0]:
-                print("** class name missing **")
-            try:
-                s[1]
-            except Exception:
-                print("** instance id missing **")
-            else:
-                if s[0] in classes:
-                    obj = storage.all()
-                    key = str(s[0]) + '.' + str(s[1])
-                    print(obj[key])
-                else:
-                    print("** class doesn't exist **")
-
-        """
-
+        """ print string rep of an instance based on class name/id """
         if not line:
             print("** class name missing **")
         else:
             cmd = line.split()
+            flag = 0
             if cmd[0] is None:
                 print("** class name missing **")
             elif (cmd[0] in classes):
@@ -74,8 +54,12 @@ class HBNBCommand(cmd.Cmd):
                     for key in obj.keys():
                         if (new == key):
                             print(obj[new])
+                            flag = 1
+                    if (flag == 0):
+                        print("** no instance found **")
             else:
                 print("** class doesn't exist **")
+
     def do_destroy(self, line):
         """ destroy function """
         if not line:
@@ -116,12 +100,14 @@ class HBNBCommand(cmd.Cmd):
     def do_update(self, line):
         """ updates an instance based on the class and id """
         if not line:
-            print("** class doesn't exist **")
+            print("** class name missing **")
         else:
             cmd = line.split()
             if (cmd[0] in classes):
                 if (len(cmd) < 2):
                     print("** instance id missing **")
+                elif ((str(cmd[0]) + "." + str(cmd[1])) not in storage.all().keys()):
+                        print("** no instance found **")
                 elif (len(cmd) < 3):
                     print("** attribute name missing **")
                 elif (len(cmd) < 4):
@@ -129,21 +115,21 @@ class HBNBCommand(cmd.Cmd):
                 else:
                     obj = storage.all()
                     new = str(cmd[0]) + "." + str(cmd[1])
-                    print(new)
                     for key in obj.keys():
-                        print(key)
                         if (new == key):
                             temp = obj[key]
-                            print("---------")
-                            print("temp: {}", format(temp))
-                            temp[cmd[2]] = cmd[3]
-                            #temp.update(cmd[2]: cmd[3])
+                            try:
+                                cmd[3] = cmd[3].strip('"')
+                            except:
+                                pass
+                            setattr(temp, cmd[2], cmd[3])
                             storage.save()
-                            print("----------")
-                            print("obj[key]: {}".format(obj[key]))
-                        else:
-                            print("nope")
-                    storage.save()
+                            flag = 1
+                    if (flag == 0):
+                        print("** no instance found **")
+            else:
+                print("** class doesn't exist **")
+
 
 if __name__ == '__main__':
     classes = {"BaseModel": BaseModel}
