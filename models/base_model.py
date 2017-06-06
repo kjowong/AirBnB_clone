1db1e54ab775d6e031d02348a07b6c0099aec3ab
Kimberly Wong <kjowong@gmail.com>
2017-06-06 23:19:52 +0000

    testing autopep8

--- models/base_model.py
+++ models/base_model.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 """BaseModel that defines all common attributes/methods for other classes"""
-# from datetime import datetime
 from uuid import uuid4
 from models import storage
 from datetime import datetime
-
 format = '%Y-%m-%dT%H:%M:%S.%f'
+
+
 class BaseModel():
-    "class Base Model"
+    "class BaseModel"
+
     def __init__(self, *args, **kwargs):
+        """ initialization self"""
         if kwargs:
             if 'created_at' in kwargs:
                 try:
-                    kwargs['created_at'] = datetime.strptime(kwargs['created_at'], format)
-                except:
+                    kwargs['created_at'] = datetime.strptime(
+                        kwargs['created_at'],
+                        format)
+                except BaseException:
                     pass
-            #if '__class__' in kwargs:
+            # if '__class__' in kwargs:
              #   del kwargs['__class__']
             self.__dict__ = kwargs
         else:
@@ -24,23 +28,28 @@ class BaseModel():
             storage.new(self)
 
     def save(self):
-        " updates public instance attribute with the current datetime"
+        """ updates public instance attribute with the current datetime """
         self.updated_at = datetime.now()
         storage.save()
 
     def to_json(self):
-        " returns a dict containing all keys\values of the __dict __"
+        """ returns a dict containing all keys\values of the __dict __ """
         new_dict = self.__dict__.copy()
         new_dict.update({'__class__': str(self.__class__.__name__)})
-        new_dict.update({'created_at': datetime.strftime((self.created_at), format)})
+        new_dict.update(
+            {'created_at': datetime.strftime((self.created_at), format)})
         try:
-            new_dict.update({'updated_at': datetime.strftime((self.updated_at), format)})
-        except:
+            new_dict.update(
+                {'updated_at': datetime.strftime((self.updated_at), format)})
+        except BaseException:
             pass
         return new_dict
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """ printing string representation of BaseModel """
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def __repr__(self):
+        """ format __str__ """
         return self.__str__()
