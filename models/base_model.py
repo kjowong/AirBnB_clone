c18e1b1530c562bc2131526533071e02706cbc2f
Halina Veratsennik <121@holbertonschool.com>
2017-06-06 20:17:18 +0000

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 """BaseModel that defines all common attributes/methods for other classes"""
-import datetime
-import uuid
+from datetime import datetime
+from uuid import uuid4
 
 
 class BaseModel:
     "class Base Model"
-
+    def __init__(self, *args, **kwargs):
+        self.id = str(uuid4())
+        self.created_at = str(datetime.now())
 
     def save(self):
         " updates public instance attribute with the current datetime"
+        self.updated_at = str(datetime.now())
 
     def to_json(self):
         " returns a dict containing all keys\values of the __dict __"
 
     def __str__(self):
-        
+        return()
