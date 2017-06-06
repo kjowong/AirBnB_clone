caae38cf373b64048d94095f74c513227fbacac1
Kimberly Wong <kjowong@gmail.com>
2017-06-06 01:31:31 +0000

    Adding check for updated/create

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 # from datetime import datetime
 from uuid import uuid4
 from models import storage
-import datetime
+from datetime import datetime
 
 format = '%Y-%m-%dT%H:%M:%S.%f'
 class BaseModel():
@@ -15,21 +15,23 @@ class BaseModel():
            # self.updated_at = datetime.now()
         else:
             self.id = str(uuid4())
-            self.created_at = datetime.datetime.now()
+            self.created_at = datetime.now()
             storage.new(self)
 
     def save(self):
         " updates public instance attribute with the current datetime"
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
         storage.save()
 
     def to_json(self):
         " returns a dict containing all keys\values of the __dict __"
         new_dict = self.__dict__.copy()
         new_dict.update({'__class__': str(self.__class__.__name__)})
-        new_dict.update({'created_at': datetime.datetime.strftime((self.created_at), format)})
-        new_dict.update({'updated_at': datetime.datetime.strftime((self.updated_at), format)})
-
+        new_dict.update({'created_at': datetime.strftime((self.created_at), format)})
+        try:
+            new_dict.update({'updated_at': datetime.strftime((self.updated_at), format)})
+        except:
+            pass
         return new_dict
 
     def __str__(self):
