12f16db59c86088c64d159637e30eb573fbad11e
Kimberly Wong <kjowong@gmail.com>
2017-06-06 22:46:40 +0000

    removing merge conflict

--- tests/base_model_test.py
+++ tests/base_model_test.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """ unittest for basemode """
 
 import unittest
@@ -18,23 +17,3 @@ class TestBaseModel(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-=======
-
-import unittest
-import models
-from models import base_model
-from models.base_model import BaseModel
-
-class BaseModelTest(unittest.TestCase):
-
-    def test_pass(self):
-        self.assertTrue(True)
-
-    def test_fail(self):
-        self.assertTrue(False)
-
-
-if __name__ == '__main__':
-    unittest.main()
->>>>>>> master
