7484ec65bbd68f2e6135452369f9a1be1178fd55
Kimberly Wong <kjowong@gmail.com>
2017-06-06 22:18:08 +0000

    Uncommneting amenity and review classes

--- console.py
+++ console.py
@@ -8,9 +8,9 @@ from models.engine.file_storage import FileStorage
 from models.user import User
 from models.state import State
 from models.city import City
-# from models.amenity import Amenity
+from models.amenity import Amenity
 from models.place import Place
-# from models.review import Review
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
@@ -143,9 +143,5 @@ class HBNBCommand(cmd.Cmd):
 
 if __name__ == '__main__':
     classes = {"BaseModel": BaseModel, "User": User, "State": State,
-               "City": City, "Place": Place}
-    """
-    "Amenity": Amenity,
-    "Review": Review
-    """
+            "City": City, "Place": Place, "Amenity": Amenity, "Review": Review}
     HBNBCommand().cmdloop()
