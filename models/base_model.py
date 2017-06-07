e29e13ab341787f4dcecea5e43647eb1f157c213
Kimberly Wong <kjowong@gmail.com>
2017-06-07 11:05:16 +0000

    removing try except

--- models/base_model.py
+++ models/base_model.py
@@ -12,11 +12,14 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """ initialization self"""
         if kwargs:
-            if 'created_at' in kwargs:
+            kwargs['created_at'] = datetime.strptime(
+                kwargs['created_at'],
+                self.format)
+            if 'updated_at' in kwargs:
                 try:
-                    kwargs['created_at'] = datetime.strptime(
-                        kwargs['created_at'],
-                        self.format)
+                    kwargs['updated_at'] = datetime.strptime(
+                        kwargs['updated_at'],
+                        self.format]
                 except:
                     pass
             if '__class__' in kwargs:
