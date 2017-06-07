6efc3988c9e3cd29e64b6a65e2f8a9e249d5029c
Kimberly Wong <kjowong@gmail.com>
2017-06-07 10:47:51 +0000

    Adding condition for class

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """BaseModel that defines all common attributes/methods for other classes"""
-import json
 from uuid import uuid4
 from models import storage
 from datetime import datetime
 format = '%Y-%m-%dT%H:%M:%S.%f'
 
-
-class BaseModel():
-    "class BaseModel"
+class BaseModel:
+    """"class BaseModel"""
 
     def __init__(self, *args, **kwargs):
         """ initialization self"""
@@ -20,8 +18,8 @@ class BaseModel():
                         format)
                 except:
                     pass
-            # if '__class__' in kwargs:
-            #   del kwargs['__class__']
+            if '__class__' in kwargs:
+                del kwargs['__class__']
             self.__dict__ = kwargs
         else:
             self.id = str(uuid4())
