0823a0e950eefaa5226796367e9d835ea235e284
Kimberly Wong <kjowong@gmail.com>
2017-06-07 20:18:43 +0000

    adding doctests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -7,7 +7,6 @@ import os
 from models import storage
 from models.engine.file_storage import FileStorage
 
-
 class TestBaseModelDictStorage(unittest.TestCase):
     """ class testing File Storage """
 
@@ -26,6 +25,39 @@ class TestBaseModelDictStorage(unittest.TestCase):
     def test_file_storage(self):
         """ test for filestorage """
         self.assertFalse(hasattr(self.ikea, "fake_id"))
+
+class TestDocumentation(unittest.TestCase):
+    """ class to test documentation """
+
+    def test_doc_class(self):
+        """ test class """
+        expected = ' filestorage class '
+        actual = FileStorage.__doc__
+        self.assertEqual(expected, actual)
+
+    def test_doc_all(self):
+        """ test all function """
+        expected = ' defining all '
+        actual = FileStorage.all.__doc__
+        self.assertEqual(expected, actual)
+
+    def test_doc_new(self):
+        """ test new function """
+        expected = ' defining new '
+        actual = FileStorage.new.__doc__
+        self.assertEqual(expected, actual)
+
+    def test_doc_save(self):
+        """ test class """
+        expected = ' defining save '
+        actual = FileStorage.save.__doc__
+        self.assertEqual(expected, actual)
+
+    def test_doc_reload(self):
+        """ test reload function """
+        expected = ' defining reload '
+        actual = FileStorage.reload.__doc__
+        self.assertEqual(expected, actual)
 
 
 if __name__ == '__main__':
