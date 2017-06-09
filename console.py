d9c81bdfdd0f2f5e7a96b7a269a05c4c7a88551b
Halina Veratsennik <121@holbertonschool.com>
2017-06-09 17:41:18 +0000

    create for console

--- console.py
+++ console.py
@@ -2,14 +2,17 @@
 """ Entry point to command line interpretor """
 import cmd
 import os
-
+from models.base_model import BaseModel
+from models.__init__ import storage
 
 class HBNBCommand(cmd.Cmd):
     """ entry point to hbnb"""
+#    classes = {"BaseModel": BaseModel}
 #    intro = "Simple command line interpretor"
-    prompt = '(hbnb) \n'
+    prompt = '(hbnb) '
 
     def do_quit(self, line):
+        """Quit command to exit the program """
         return True
 
     def do_EOF(self, line):
@@ -19,5 +22,18 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         return False
 
+    def do_create(self, line):
+        if not line:
+            print("** class name missing **")
+        else:
+            cmd = line.split()
+            if (cmd[0] in classes):
+                inst = classes[cmd[0]]()
+                inst.save()
+                print(inst.id)
+            else:
+                print("** class doesn't exist **")
+
 if __name__ == '__main__':
+    classes = {"BaseModel": BaseModel}
     HBNBCommand().cmdloop()
