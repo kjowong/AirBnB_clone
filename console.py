0e7d4e03a5fd2a808916b5203e5b5a5341b419b6
Halina Veratsennik <121@holbertonschool.com>
2017-06-11 02:00:04 +0000

    user class

--- console.py
+++ console.py
@@ -5,6 +5,13 @@ import os
 from models.base_model import BaseModel
 from models  import storage
 from models.engine.file_storage import FileStorage
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
 
 class HBNBCommand(cmd.Cmd):
     """ entry point to hbnb"""
@@ -132,5 +139,8 @@ class HBNBCommand(cmd.Cmd):
 
 
 if __name__ == '__main__':
-    classes = {"BaseModel": BaseModel}
+    classes = {"BaseModel": BaseModel, "User": User,
+               "State": State, "City": City,
+               "Amenity": Amenity, "Place": Place,
+               "Review": Review}
     HBNBCommand().cmdloop()
---
+++ models/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" creating User Class """
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """ Class User """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__ (self, *args, **kwargs):
+        """ initialization """
+        if kwargs is not None:
+            self.__dict__ = kwargs
+        else:
+            super().__init__(self)
