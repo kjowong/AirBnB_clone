2c2284ef7e7076a0b7f113d0ceff68297d064a2c
Halina Veratsennik <121@holbertonschool.com>
2017-06-12 20:31:30 +0000

    test amenity

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Amenity Class Unittest """
+import unittest
+import models
+from models.amenity import Amenity
+
+
+class TestAmenityClass(unittest.TestCase):
+    """ testing amenity class """
+
+    def test_amenity_attr(self):
+        """ amenity test """
+        self.one = Amenity()
+        self.assertTrue(hasattr(self.one, "name"))
+        self.assertFalse(hasattr(self.one, "area"))
+
+    def test_amenity_value(self):
+        self.one = Amenity()
+        self.assertEqual(self.one.name, "")
+        self.assertFalse(self.one.name, "under the bridge")
+
+    def test_amenity_type(self):
+        self.one = Amenity()
+        self.assertEqual(type(self.one.name), str)
+        self.assertNotEqual(type(self.one.name), list)
+
+if __name__ == '__main__':
+    unittest.main()
