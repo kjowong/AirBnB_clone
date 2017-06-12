4e36770e36a51c8a22fa1ddc702b74a7490acdb0
Halina Veratsennik <121@holbertonschool.com>
2017-06-12 15:01:31 +0000

    user test updated

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -14,6 +14,10 @@ class TestUserClass(unittest.TestCase):
         self.assertTrue(hasattr(self.one, "first_name"))
         self.assertTrue(hasattr(self.one, "last_name"))
         self.assertFalse(hasattr(self.one, "middle_name"))
+        self.assertTrue(hasattr(self.one, "email"))
+        self.assertTrue(hasattr(self.one, "password"))
+        self.assertEqual(self.one.first_name, "")
+        self.assertFalse(self.one.email, "hbnb@holberton.com")
 
 if __name__ == '__main__':
     unittest.main()
